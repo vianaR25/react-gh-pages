6a508b770fb1230717dbdd712c50a481e24c0172
Eduardo Mangeli <eduardo@mangeli.com.br>
2023-11-12 21:35:09 -0300

    incluindo referência cruzada

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App() {
     <>
     <h1>Entrada</h1>
     <Link to={'/outra'}>Outra</Link>
+    <div style={{'margin-top': '5em'}}>
+      <Link to={'https://github.com/EduardoMangeli/react-github-pages'}>Tutorial para incluir uma aplicação react no github pages</Link>  
+    </div>
     </>
   );
 }
